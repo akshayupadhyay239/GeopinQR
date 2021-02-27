17e5ee74b57edbc95055f0651796ce242c1513a1
akshayupadhyay <akshayupadhyay239@gmai.com>
2021-02-27 16:09:57 +0530

    changing to pure JS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
 //importing all the components
 import Qrlayout from './components/QRlayout';
 import Illustration from './components/Illustration';
-import MapContainer from './components/Map';
+import Map from './components/Map';
 
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {
-      longitude:37.76,
-      latitude:-122.4469157
-    }
-  }
-
-  render(){return (
+function App() {
+  return (
     <div className="App">
       <Illustration />
       <div className="main">
         <Qrlayout />
-      </div> 
-     <Map google={this.props.google}/>
+      </div>
+      <Map />
     </div>
   );
-  }
 }
 
 export default App;

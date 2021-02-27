7e68795a586a56fb703eda3b326af655569e3c93
akshayupadhyay <akshayupadhyay239@gmai.com>
2021-02-27 15:29:57 +0530

    we are shifting to pure js maybe

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function Map(){
-    return(
-        <div className="map-area">
-            
-        </div>
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: -1.2884,
+            lng: 36.8233
+          }
+        }
+      />
     );
+  }
 }
-export default Map;
+
+export default MapContainer;

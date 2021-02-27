29fc4812c27f436cf26985cd512b2861c6379232
akshayupadhyay <akshayupadhyay239@gmai.com>
2021-02-27 13:48:14 +0530

    qr code layout done

---
+++ src/components/QRlayout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Qrlayout(){
+    return (
+    <div className="qr-layout">
+        <h1>Dummy Name</h1>
+        <p>dummy details about company/shop</p>
+        <div className="qr">
+            <img alt="QR code here" src="" />
+        </div>
+    </div>
+    );
+}
+
+export default Qrlayout;
